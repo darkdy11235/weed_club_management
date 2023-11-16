fad355544b6d6c40ad05e9e0891b3be6fd5f252d
CaoTuanKiett <tuankiet06032002@gmail.com>
2023-11-16 21:19:25 +0700

    setupp

---
+++ WC_FE/nuxt.config.ts
@@ -0,0 +1,71 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  runtimeConfig: {
+    public: {
+      email: process.env.NUXT_EMAIL,
+      API_BASE_BE: process.env.API_BASE_BE,
+    },
+  },
+  devtools: { enabled: true },
+  webpack: {
+    loaders: {
+      vue: {
+        hotReload: true,
+      },
+    },
+  },
+  //for tailwind
+  css: ["~/index.css", "@fortawesome/fontawesome-svg-core/styles.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  //noti nuxt3
+   build:{
+    transpile: ['vue-toastification']
+  },
+
+
+  //vuetify
+  modules: ["@invictus.codes/nuxt-vuetify", "@pinia/nuxt"],
+
+  vuetify: {
+    /* vuetify options */
+    vuetifyOptions: {
+      theme: {
+        themes: {
+          light: {
+            dark: false,
+            colors: {
+              background: "#F5F5F5",
+              primary: "#4A85F6",
+              surface: "#FFFFFF",
+              secondary: "#000000",
+            },
+          },
+          dark: {
+            colors: {
+              background: "",
+            },
+          },
+        },
+      },
+      // @TODO: list all vuetify options
+    },
+    moduleOptions: {
+      /* nuxt-vuetify module options */
+      treeshaking: false,
+      useIconCDN: true,
+
+      /* vite-plugin-vuetify options */
+      styles: true,
+      autoImport: true,
+      useVuetifyLabs: true,
+      /* nuxt-vuetify module options */
+    },
+  },
+});
