2276f8a5a001823d547db3f365d884bfa662c1bf
DARKDY <darkdy11235@gmail.com>
2023-11-25 21:56:43 +0700

    fix: auth

--- WC_BE/routes/web.php
+++ WC_BE/routes/web.php
@@ -3,19 +3,33 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Web routes for user management
+Route::group(['middleware' => ['web']], function () {
+    // Example: Display a list of users
+    Route::get('/users', [UserController::class, 'index']);
 
-Route::get('/', function () {
-    return to_route('users.index');
+    // Example: Display the form to create a new user
+    Route::get('/users/create', [UserController::class, 'create']);
+
+    // Example: Store a new user
+    Route::post('/users', [UserController::class, 'store']);
+
+    // Other user-related routes can be added here...
 });
 
-Route::resource('/users', UserController::class);
+// Additional web routes can be added outside the group
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Example: Route with a named middleware
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+// Example: Route with individual middleware
+Route::get('/profile', function () {
+    return view('profile');
+})->middleware('auth')->name('profile');
