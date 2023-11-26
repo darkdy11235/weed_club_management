4e7de02279c6332fee7c809cd7866710b771df2d
Thanhhung <anhkp222@gmail.com>
2023-11-26 23:08:04 +0700

    handle payment bill controller

--- WC_BE/app/Models/Bill.php
+++ WC_BE/app/Models/Bill.php
@@ -14,6 +14,7 @@ class Bill extends Model
      *
      * @var array
      */
+
     protected $fillable = [
         'fee_type',
         'payer',
@@ -24,4 +25,71 @@ class Bill extends Model
         'year',
         'description'
     ];
+
+    public function payment()
+    {
+        return $this->belongsTo(Payment::class);
+    }
+
+    public static function createBill($bill)
+    {
+        $created_at = now();
+        $bill_at = now()->addDays(30);
+
+        $newBills = [];
+
+        foreach ($bill['payers'] as $payer) {
+            $newBill = Bill::create([
+                'fee_type' => $bill['fee_type'],
+                'fee' => $bill['fee'],
+                'created_at' => $created_at,
+                'bill_at' => $bill_at,
+                'description' => $bill['description'],
+                'created_by' => $bill['create_by'], 
+                'payer' => $payer,
+                'year' => $bill['year'],
+                'month' => $bill['month'],
+            ]);
+
+            $newBills[] = $newBill;
+        }
+
+        return $bill;
+    }
+
+    public static function updateBill($newBill)
+    {
+        $updatedBill = Bill::where('id', $newBill['id'])->update([
+            'fee_type' => $newBill['fee_type'],
+            'fee' => $newBill['fee'],
+            'description' => $newBill['description'],
+            'payer' => $newBill['payer'],
+            'year' => $newBill['year'],
+            'month' => $newBill['month'],
+        ]);
+
+        return $updatedBill;
+    }
+
+    public static function getAllBill()
+    {
+        $data = Bill::select('bills.*', 'users.name')
+            ->join('users', 'bills.payer', '=', 'users.id')
+            ->orderBy('id', 'desc')
+            ->get();
+
+        return $data;
+    }
+
+    public static function getBillById($id){
+        $data = Bill::where('id', $id)->get();
+
+        return $data;
+    }
+
+    public static function deleteBill($id) {
+        $deletedBill = Bill::where('id', $id)->delete();
+
+        return $deletedBill;
+    }
 }
--- WC_BE/routes/api.php
+++ WC_BE/routes/api.php
@@ -1,11 +1,29 @@
 <?php
 
+use App\Http\Controllers\API\BillController;
 use App\Http\Controllers\API\LoginController;
+use App\Http\Controllers\API\PaymentController;
 use App\Http\Controllers\API\RegistrationController;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\TestTroller;
 
 Route::post('/register', [RegistrationController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
+
+Route::prefix('payment')->group(function () {
+  Route::get('all', [PaymentController::class, 'getAllPayments']);
+  Route::get('user/{id}', [PaymentController::class, 'getPaymentsByUserId']);
+  Route::get('detail/{paymentId}', [PaymentController::class, "getPaymentDetail"]);
+});
+
+Route::prefix('bill')->group(function(){
+  Route::get("all", [BillController::class, "getAllBill"]);
+  Route::get("read/{id}", [BillController::class, "getBillById"]);
+  Route::post('create', [BillController::class, "createBill"]);
+  Route::put('update', [BillController::class, "updateBill"]);
+  Route::delete("delete/{id}", [BillController::class, "deleteBill"]);
+});
+
 Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'getUser']);
 Route::middleware('auth:sanctum')->post('/logout', [UserController::class, 'logout']);
 
