6c6bd60bc8d417993f862a051b93ad4705d8cb58
DARKDY <darkdy11235@gmail.com>
2023-11-26 19:12:50 +0700

    feat: done auth

--- WC_BE/app/Http/Controllers/API/UserController.php
+++ WC_BE/app/Http/Controllers/API/UserController.php
@@ -2,21 +2,35 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
+    /**
+     * Get the authenticated user.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getUser()
     {
         $user = Auth::user();
-        return response()->json($user);
+
+        return response()->json(['user' => $user]);
     }
 
+    /**
+     * Logout the authenticated user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function logout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
+
         return response()->json(['message' => 'Successfully logged out']);
     }
 }
+
